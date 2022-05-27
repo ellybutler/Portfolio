ead0df269b9c78bb297a6bd33c6315484ffc0ad5
El Butler <elvia@outlook.com>
2022-05-27 15:52:46 -0500

    Bootstrap & MD install

--- porfolio-md/src/app/app.component.spec.ts
+++ porfolio-md/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-cli'`, () => {
+  it(`should have as title 'porfolio-md'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio-cli');
+    expect(app.title).toEqual('porfolio-md');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio-cli app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('porfolio-md app is running!');
   });
 });
--- porfolio-md/src/app/app.component.ts
+++ porfolio-md/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio-cli';
+  title = 'Elly\'s Portfolio';
 }
